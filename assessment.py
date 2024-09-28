26a881f8651e3523ee5a1644d9ac673833dd08bd
Andrew Valentin <andrewvalentin64@gmail.com>
2024-09-28 02:51:52 -0400

    updated assessment method

--- assessment.py
+++ assessment.py
@@ -9,33 +9,48 @@ speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_r
 audio_config = speechsdk.AudioConfig(use_default_microphone=True)
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language=language, audio_config=audio_config)
 
-pronunciation_config = speechsdk.PronunciationAssessmentConfig( 
-    reference_text="", 
-    grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark, 
-    granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme, 
-    enable_miscue=False) 
-pronunciation_config.enable_prosody_assessment() 
-pronunciation_config.enable_content_assessment_with_topic("greeting")
-pronunciation_config.apply_to(speech_recognizer)
+def getAssessment(phrase: str):
+    pronunciation_config = speechsdk.PronunciationAssessmentConfig( 
+        reference_text=phrase, 
+        grading_system=speechsdk.PronunciationAssessmentGradingSystem.HundredMark, 
+        granularity=speechsdk.PronunciationAssessmentGranularity.Phoneme, 
+        enable_miscue=False) 
+    pronunciation_config.enable_prosody_assessment() 
+    #pronunciation_config.enable_content_assessment_with_topic("greeting")
+    pronunciation_config.apply_to(speech_recognizer)
 
-# (Optional) get the session ID
-speech_recognizer.session_started.connect(lambda evt: print(f"SESSION ID: {evt.session_id}"))
+    # (Optional) get the session ID
+    speech_recognizer.session_started.connect(lambda evt: print(f"SESSION ID: {evt.session_id}"))
 
-print('Say something...')
-result = speech_recognizer.recognize_once()
-print('done.')
+    print('Say something...')
+    result = speech_recognizer.recognize_once()
+    print('done.')
 
-# The pronunciation assessment result as a Speech SDK object
-pronunciation_assessment_result = speechsdk.PronunciationAssessmentResult(result)
+    # The pronunciation assessment result as a Speech SDK object
+    pronunciation_assessment_result = speechsdk.PronunciationAssessmentResult(result)
+    acc_score = pronunciation_assessment_result.accuracy_score
+    result_str = phrase + ' : ' + str(acc_score) + '\n'
+    words = pronunciation_assessment_result.words
 
-# The pronunciation assessment result as a JSON string
-pronunciation_assessment_result_json = result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
-pronunciation_assessment_result_data = json.loads(pronunciation_assessment_result_json)
+    for word in words:
+        result_str += str(word.word) + ' : ' + str(word.accuracy_score) + '\n'
 
-sentence = pronunciation_assessment_result_data['NBest'][0]['Lexical']
-accuracy_overall = pronunciation_assessment_result_data['NBest'][0]['PronunciationAssessment']['AccuracyScore']
-words = pronunciation_assessment_result_data['NBest'][0]['Words']
+    '''
+    # The pronunciation assessment result as a JSON string
+    pronunciation_assessment_result_json = result.properties.get(speechsdk.PropertyId.SpeechServiceResponse_JsonResult)
+    pronunciation_assessment_result_data = json.loads(pronunciation_assessment_result_json)
 
-print(sentence, ':', accuracy_overall)
-for word in words:
-    print(word['Word'], ':', word['PronunciationAssessment']['AccuracyScore'])
+    sentence = pronunciation_assessment_result_data['NBest'][0]['Lexical']
+    accuracy_overall = pronunciation_assessment_result_data['NBest'][0]['PronunciationAssessment']['AccuracyScore']
+    words = pronunciation_assessment_result_data['NBest'][0]['Words']
+
+    print(sentence, ':', accuracy_overall)
+    for word in words:
+        print(word['Word'], ':', word['PronunciationAssessment']['AccuracyScore'])
+    
+    print(pronunciation_assessment_result_data)
+    '''
+    return result_str
+
+result = getAssessment("Hello. How are you doing today?")
+print(result)
